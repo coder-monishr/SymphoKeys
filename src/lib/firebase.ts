0c536fda7c6210b8f233d7425134059c2c7634ea
Monish R <112107452+coder-monishr@users.noreply.github.com>
2025-10-18 12:41:14 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,10 +2,10 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  projectId: 'studio-7718316245-b67b6',
-  appId: '1:985223662347:web:5a59cb6d866781dfc4b00e',
-  apiKey: 'AIzaSyBV55MdwWucj-lNRQgbyJKvggGFypw3i5A',
-  authDomain: 'studio-7718316245-b67b6.firebaseapp.com',
+  projectId: 'projectID',
+  appId: 'app id here!',
+  apiKey: 'your api key here!',
+  authDomain: 'Fill your domain',
   measurementId: '',
   messagingSenderId: '985223662347',
 };
